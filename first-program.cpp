1ca2ee6323ce962eb251c5fb7dad9b34322c4090
Wilson Widyadhana <wilsonwidyadhana681@gmail.com>
2021-12-03 21:18:13 +0800

    added quite a lot of things

--- first-program.cpp
+++ first-program.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
 
 int main(){
-    std::cout << "Enter your favourite number between 1-100: ";
+    int favourite_number;
 
+    std::cout << "Enter your favourite number between 1-100: ";
+    std::cin >> favourite_number;
+    std::cout << "Amazing! That's my favourite number too!" << std::endl;
+
+    return 0;
 }

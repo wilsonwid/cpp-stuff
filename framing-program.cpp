b67ecb03e873fb77b3aaff2f1d7777b44acb80fb
Wilson Widyadhana <wilsonwidyadhana681@gmail.com>
2022-06-26 18:41:47 +0800

    Improved on program

--- framing-program.cpp
+++ framing-program.cpp
@@ -7,27 +7,23 @@ int main() {
   cout << "Please enter your first name: ";
   string name;
   cin >> name;
+  cout << "Please enter how much padding you want: ";
+  int pad;
+  cin >> pad;
 
   const string greeting = "Hello, " + name + "!";
-  const int pad = 1;
   const int rows = pad*2 +3;
   const string::size_type cols = greeting.size() + pad*2 + 2;
   cout << "\n";
-  
+
   for (int r = 0; r < rows; ++r) {
     string::size_type c = 0;
-    while (c < cols) {
-      if (r == pad+1 && c == pad+1) {
-        cout << greeting;
-        c += greeting.size();
-      } else {
-        if (r == 0 || r == rows-1 || c == 0 || c== cols-1) {
-          cout << "*";
-        } else {
-          cout << " ";
-        }
-        ++c;
-      } 
+    if (r == pad+1) { 
+      cout << "*" + string(pad, ' ') + greeting + string(pad, ' ') + "*";
+    } else if (r == 0 || r == rows-1) {
+      cout << string(cols, '*');
+    } else {
+      cout << "*" + string(cols-2, ' ') + "*";
     }
     cout << "\n";
   }
